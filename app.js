093ffaaf1afa2f0aaa2de54809e83ecc702d3f6f
Nathan Borges de Jesus <borgesn096@gmail.com>
2025-02-24 23:20:38 -0300

    Sortear numero secreto - começo

---
+++ app.js
@@ -0,0 +1,10 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+}
+
+function numeroSoteado(){
+    return Math.floor
+}
